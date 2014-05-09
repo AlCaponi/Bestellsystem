e0aa50afaa9577c05fdaa8e1832fbfc654a23644
Angelo Conconi <angelo.conconi@gmail.com>
2014-05-09 13:49:23 +0200

    Service demo implementation

--- src/ch/hslu/dmg/service/BestellService.java
+++ src/ch/hslu/dmg/service/BestellService.java
@@ -1,17 +1,27 @@
 package ch.hslu.dmg.service;
 
+import ch.hslu.dmg.Dataaccess.PersonDAO;
 import ch.hslu.dmg.library.collection.*;
 
 /**
  * @author Angelo on 08.05.2014.
  */
 public class BestellService {
+    private PersonDAO personDAO;
+
+    public BestellService()
+    {
+        personDAO = new PersonDAO();
+    }
+
     public PersonCol GetAllPersonen(){
-        return new PersonCol();
+        return personDAO.readPersonen();
     }
     public MitarbeiterCol GetAllMitarbeiter() { return new MitarbeiterCol(); }
     public MaschineCol GetAllMaschine(){return new MaschineCol();}
     public FertigungsteilCol GetAllFertigungsteil(){return new FertigungsteilCol();}
     public FertigungsschrittCol GetAllFertigungsschritt() {return new FertigungsschrittCol();}
     public TeilCol GetAllTeil(){return new TeilCol();}
+
+
 }
